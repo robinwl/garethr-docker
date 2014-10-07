f121c1169a8b1d32293cc2c756573647a05aadfc
Robin Westin Larsson <robin@rwl.se>
2014-10-07 21:56:57 +0200

    Added param for pull_on_start. Implemented pull_on_start for upstart.

--- manifests/run.pp
+++ manifests/run.pp
@@ -23,6 +23,7 @@ define docker::run(
   $disable_network = false,
   $privileged = false,
   $extra_parameters = undef,
+  $pull_on_start = false
 ) {
   include docker::params
   $docker_command = $docker::params::docker_command
